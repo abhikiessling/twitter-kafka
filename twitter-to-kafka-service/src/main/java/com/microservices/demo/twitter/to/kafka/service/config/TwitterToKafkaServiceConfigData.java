8f2353a00f1024d6c6b3c5bb689e958ee01764a7
Abhishek (Shubham) <abhi2495@outlook.com>
2021-04-18 05:50:34 +0530

    initial commit

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
@@ -0,0 +1,28 @@
+package com.microservices.demo.twitter.to.kafka.service.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfigData {
+    private List<String> twitterKeywords;
+    private String welcomeMessage;
+    
+	public List<String> getTwitterKeywords() {
+		return twitterKeywords;
+	}
+	public void setTwitterKeywords(List<String> twitterKeywords) {
+		this.twitterKeywords = twitterKeywords;
+	}
+	public String getWelcomeMessage() {
+		return welcomeMessage;
+	}
+	public void setWelcomeMessage(String welcomeMessage) {
+		this.welcomeMessage = welcomeMessage;
+	}
+}
